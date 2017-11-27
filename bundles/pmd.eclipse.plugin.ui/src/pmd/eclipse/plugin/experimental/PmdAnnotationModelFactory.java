2ba70380715bba44bf880c662bef413b51409d2d
Christian Wulf <chw@informatik.uni-kiel.de>
2017-11-27 22:47:08 +0100

    closes #29

--- bundles/pmd.eclipse.plugin.ui/src/pmd/eclipse/plugin/experimental/PmdAnnotationModelFactory.java
+++ bundles/pmd.eclipse.plugin.ui/src/pmd/eclipse/plugin/experimental/PmdAnnotationModelFactory.java
@@ -1,8 +1,15 @@
 package pmd.eclipse.plugin.experimental;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.ui.texteditor.ResourceMarkerAnnotationModelFactory;
 
 // extension point: org.eclipse.core.filebuffers.annotationModelCreation
 public class PmdAnnotationModelFactory extends ResourceMarkerAnnotationModelFactory {
 
+	@Override
+	public IAnnotationModel createAnnotationModel(IPath location) {
+		// TODO Auto-generated method stub
+		return super.createAnnotationModel(location);
+	}
 }
