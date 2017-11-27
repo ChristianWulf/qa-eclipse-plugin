b331e155b30c0274e91e0d9fff0637b637f33771
Christian Wulf <chw@informatik.uni-kiel.de>
2017-11-27 18:26:01 +0100

    removed annotation in overview ruler

---
+++ bundles/pmd.eclipse.plugin.ui/src/pmd/eclipse/plugin/experimental/PmdAnnotationModelFactory.java
@@ -0,0 +1,8 @@
+package pmd.eclipse.plugin.experimental;
+
+import org.eclipse.ui.texteditor.ResourceMarkerAnnotationModelFactory;
+
+// extension point: org.eclipse.core.filebuffers.annotationModelCreation
+public class PmdAnnotationModelFactory extends ResourceMarkerAnnotationModelFactory {
+
+}
