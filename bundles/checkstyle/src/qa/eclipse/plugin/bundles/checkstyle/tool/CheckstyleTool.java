ee3ec623a0de65584f58a5cd0dcfefd7105ba9fd
Christian Wulf <chw@informatik.uni-kiel.de>
2018-02-03 22:47:37 +0100

    first prototype command

--- bundles/checkstyle/src/qa/eclipse/plugin/bundles/checkstyle/tool/CheckstyleTool.java
+++ bundles/checkstyle/src/qa/eclipse/plugin/bundles/checkstyle/tool/CheckstyleTool.java
@@ -1,6 +1,7 @@
 package qa.eclipse.plugin.bundles.checkstyle.tool;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
@@ -17,56 +18,73 @@ public class CheckstyleTool implements AuditListener {
 	public CheckstyleTool() {
 		checker = new Checker();
 	}
-	
-	public void startAsyncAnalysis(List<IFile> value) {
-//		checker.setBasedir(basedir);
-//		checker.setCacheFile(fileName);
-//		checker.setCharset(arg0);
-//		checker.setClassLoader(classLoader);
+
+	public void startAsyncAnalysis(List<IFile> eclipseFiles) {
+		// checker.setBasedir(basedir);
+		// checker.setCacheFile(fileName);
+		// checker.setCharset(arg0);
+		// checker.setClassLoader(classLoader);
 		checker.addListener(this);
-		
-		List<File> files = null;
-		try {
-			int numViolations = checker.process(files);
-		} catch (CheckstyleException e) {
-			throw new IllegalStateException(e);		// log to error view somewhere
+
+		for (IFile eclipseFile : eclipseFiles) {
+			// if (monitor.isCanceled()) {
+			// // only stop the loop, not the whole method to finish reporting
+			// break;
+			// }
+
+			if (!eclipseFile.isAccessible()) {
+				continue;
+			}
+
+			final File sourceCodeFile = eclipseFile.getRawLocation().makeAbsolute().toFile();
+
+			List<File> files = Arrays.asList(sourceCodeFile);
+			int numViolations;
+
+			try {
+				numViolations = checker.process(files);
+			} catch (CheckstyleException e) {
+				throw new IllegalStateException(e); // log to error view somewhere
+			}
+
+			System.out.println("numViolations: " + numViolations);
 		}
 	}
 
 	@Override
 	public void addError(AuditEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addException(AuditEvent arg0, Throwable arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void auditFinished(AuditEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void auditStarted(AuditEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void fileFinished(AuditEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void fileStarted(AuditEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
