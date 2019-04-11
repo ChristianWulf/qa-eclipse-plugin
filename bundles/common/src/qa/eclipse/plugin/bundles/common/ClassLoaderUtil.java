4bdcf03f8a013b6a89bb1ab51ddb673cffa67c41
Reiner Jung <reiner.jung@email.uni-kiel.de>
2019-04-11 13:56:29 +0200

    Merged updates

---
+++ bundles/common/src/qa/eclipse/plugin/bundles/common/ClassLoaderUtil.java
@@ -0,0 +1,29 @@
+package qa.eclipse.plugin.bundles.common;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.function.Supplier;
+
+public final class ClassLoaderUtil {
+
+	private ClassLoaderUtil() {
+		// utility class
+	}
+
+	public static URLClassLoader newClassLoader(URL[] urls, ClassLoader parentClassLoader) {
+		URLClassLoader osgiClassLoaderWithUrls = new URLClassLoader(urls, parentClassLoader);
+		return osgiClassLoaderWithUrls;
+	}
+
+	public static <T> T executeWithContextClassLoader(ClassLoader classLoader, Supplier<T> function) {
+		Thread currentThread = Thread.currentThread();
+		ClassLoader oldClassLoader = currentThread.getContextClassLoader();
+
+		currentThread.setContextClassLoader(classLoader);
+		try {
+			return function.get();
+		} finally {
+			currentThread.setContextClassLoader(oldClassLoader);
+		}
+	}
+}
